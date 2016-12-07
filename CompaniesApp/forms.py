3ee58ec39a75de78adaff71b1b38083989e7bd0b
EDawg878 <EDawg878@gmail.com>
2016-12-06 22:41:55 -0500

    Move Engineer to CompaniesApp

--- CompaniesApp/forms.py
+++ CompaniesApp/forms.py
@@ -10,3 +10,9 @@ class CompanyForm(forms.Form):
     photo = forms.ImageField(label='Photo');
     description = forms.CharField(label='Description', max_length=300)
     website = forms.CharField(label='Website', max_length = 300)
+
+class EngineerForm(forms.Form):
+	name = forms.CharField(label='Name', max_length=50)
+	alma_mater = forms.CharField(label='Alma Mater', max_length=50)
+	about = forms.CharField(label='About', max_length=300)
+	phone_number = forms.CharField(label='Phone Number', max_length=20)
--- CompaniesApp/models.py
+++ CompaniesApp/models.py
@@ -16,3 +16,13 @@ class Company(models.Model):
     
     def __str__(self):
         return self.name
+
+class Engineer(models.Model):
+	name = models.CharField(max_length=50)
+	alma_mater = models.CharField(max_length=50)
+	about = models.CharField(max_length=300)
+	phone_number = models.CharField(max_length=20)
+	projects = models.ManyToManyField(Project)
+
+	def __str__(self):
+		return self.name
